6dbd0e5dd21d3eb6ac856da015e1d903f785adb6
daniel_ovando <dovando@uade.edu.ar>
2024-09-10 16:13:18 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    # Crear una matriz de 5 filas y 4 columnas inicializada en 0
+    inventarios= [[0 for _ in range(4)] for _ in range(5)]
+
+# Imprimir la matriz
+    for fila in inventarios :
+        print(fila)
+
+main()
